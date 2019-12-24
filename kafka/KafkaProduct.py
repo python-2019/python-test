ab9d907b62cbfa0caecbdbfb4462720928df9f16
854731306@qq.com <854731306@qq.com>
2019-12-24 16:52:36 +0800

    kafka工具

--- kafka/KafkaProduct.py
+++ kafka/KafkaProduct.py
@@ -4,6 +4,7 @@ from functools import singledispatch
 
 from kafka import KafkaProducer
 
+
 @singledispatch
 def send():
     # 此处ip可以是多个['0.0.0.1:9092','0.0.0.2:9092','0.0.0.3:9092' ]
@@ -11,15 +12,17 @@ def send():
     producer.send('testTopic', str(time.localtime(time.time())).encode('utf-8'))
     producer.close()
 
+
 @send.register(str)
-def _(message):
+def _(topic, message, kafka_addr):
     # 此处ip可以是多个['0.0.0.1:9092','0.0.0.2:9092','0.0.0.3:9092' ]
-    producer = KafkaProducer(bootstrap_servers=["172.20.66.238:9092"])
-    producer.send('testTopic', message.encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers=kafka_addr)
+    producer.send(topic, message.encode('utf-8'))
     producer.close()
 
 
 if __name__ == '__main__':
-    while True:
-        send()
-        time.sleep(1)
+    topic = "testTopic"
+    message=""
+    kafka_addr=["172.20.66.238:9092"]
+    send()
