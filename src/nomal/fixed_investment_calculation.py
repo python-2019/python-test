c9b0fad8ab65c24042574f6a258f47de861c8d6f
wen.he <wen.he@timanetworks.com>
2021-04-14 18:29:43 +0800

    feat(nomal): 复利计算

--- src/nomal/fixed_investment_calculation.py
+++ src/nomal/fixed_investment_calculation.py
@@ -3,6 +3,7 @@ def get():
 
 
 def get2(years, monthMoney, rate):
+    print("累计定投: %s,每月定投: %s,月利率: %s" % (str(years),str(monthMoney),str(rate)))
     rate = rate / 12
     money = monthMoney
     for year in range(years):
